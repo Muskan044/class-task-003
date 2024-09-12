1e6642b286b485d04c4c960ef49047cc3293196f
Muskan044 <muskankhan430430@gmail.com>
2024-09-12 23:58:57 +0500

    add questions in js

---
+++ script.js
@@ -0,0 +1,52 @@
+let question = [
+    {
+        question : "Which HTML element is used to embed an image in a webpage?",
+        options : [
+            "<picture>",
+            "<img>",
+            "<figure>",
+            "<media>"
+        ],
+        answer : "<img>"
+    },
+    {
+        question : "Which HTML attribute is used to define inline styles?",
+        options : [
+            "style",
+            "<class",
+            "id",
+            "src"
+        ],
+        answer : "style"
+    },
+    {
+        question : "Which HTML element is used to create an unordered list?",
+        options : [
+            "<ol>",
+            "<li>",
+            "<ul>",
+            "<list>"
+        ],
+        answer : "<ul>"
+    },
+    {
+        question : "Which CSS property is used to make the text italic?",
+        options : [
+            "text-decoration: italic;",
+            "font-style: italic;",
+            "font-weight: italic;",
+            "text-transform: italic;"
+        ],
+        answer : "font-style: italic;"
+    },
+    {
+        question : "In CSS, how can you make an element's text bold?",
+        options : [
+            "font-style: bold;",
+            "font-weight: bold;",
+            "text-decoration: bold;",
+            "font-size: bold;"
+        ],
+        answer : "font-weight: bold;"
+    }
+]
